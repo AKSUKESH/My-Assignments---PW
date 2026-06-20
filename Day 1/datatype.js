1449cc5d65d3aae6fdc5743a6a4ae97857b7527e
AKSUKESH <155533644+AKSUKESH@users.noreply.github.com>
2026-06-20 20:45:18 +0530

    Add files via upload
    day1

---
+++ Day 1/datatype.js
@@ -0,0 +1,24 @@
+// Declare variables using let
+
+let firstName = "Sukesh";
+let companyName = "Null";
+let mobileNumber = 9876543210;
+let isAutomation = true;
+let hasPlaywright; 
+
+// Print values and their data types
+
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(companyName);
+console.log(typeof companyName);
+
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
